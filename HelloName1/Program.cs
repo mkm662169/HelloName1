6c9b904e3e3b843009f0c6a339aaad1e36bffe57
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2019-12-14 18:02:45 +0200

    Add project files.

---
+++ HelloName1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloName1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mebelo!");
+        }
+    }
+}
